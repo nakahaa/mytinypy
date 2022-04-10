143b70d2d77f577e906fb08f770f35709e71eed3
zkkxu <xufei02@pingcap.com>
2022-04-11 04:07:57 +0800

    pretty

---
+++ mypy/type.h
@@ -0,0 +1,361 @@
+#ifndef TP_H
+#define TP_H
+
+#include <setjmp.h>
+#include <sys/stat.h>
+#ifndef __USE_ISOC99
+#define __USE_ISOC99
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <math.h>
+#include <time.h>
+
+#ifdef __GNUC__
+#define tp_inline __inline__
+#endif
+
+#ifdef _MSC_VER
+#ifdef NDEBUG
+#define tp_inline __inline
+#else
+/* don't inline in debug builds (for easier debugging) */
+#define tp_inline
+#endif
+#endif
+
+#ifndef tp_inline
+#error "Unsuported compiler"
+#endif
+
+enum {
+    TP_NONE,TP_NUMBER,TP_STRING,TP_DICT,
+    TP_LIST,TP_FNC,TP_DATA,
+};
+
+typedef double tp_num;
+
+typedef struct tp_number_ {
+    int type;
+    tp_num val;
+} tp_number_;
+typedef struct tp_string_ {
+    int type;
+    struct _tp_string *info;
+    char const *val;
+    int len;
+} tp_string_;
+typedef struct tp_list_ {
+    int type;
+    struct _tp_list *val;
+} tp_list_;
+typedef struct tp_dict_ {
+    int type;
+    struct _tp_dict *val;
+    int dtype;
+} tp_dict_;
+typedef struct tp_fnc_ {
+    int type;
+    struct _tp_fnc *info;
+    int ftype;
+    void *cfnc;
+} tp_fnc_;
+typedef struct tp_data_ {
+    int type;
+    struct _tp_data *info;
+    void *val;
+    int magic;
+} tp_data_;
+
+typedef union tp_obj {
+    int type;
+    tp_number_ number;
+    struct { int type; int *data; } gci;
+    tp_string_ string;
+    tp_dict_ dict;
+    tp_list_ list;
+    tp_fnc_ fnc;
+    tp_data_ data;
+} tp_obj;
+
+typedef struct _tp_string {
+    int gci;
+    int len;
+    char s[1];
+} _tp_string;
+typedef struct _tp_list {
+    int gci;
+    tp_obj *items;
+    int len;
+    int alloc;
+} _tp_list;
+typedef struct tp_item {
+    int used;
+    int hash;
+    tp_obj key;
+    tp_obj val;
+} tp_item;
+typedef struct _tp_dict {
+    int gci;
+    tp_item *items;
+    int len;
+    int alloc;
+    int cur;
+    int mask;
+    int used;
+    tp_obj meta;
+} _tp_dict;
+typedef struct _tp_fnc {
+    int gci;
+    tp_obj self;
+    tp_obj globals;
+    tp_obj code;
+} _tp_fnc;
+
+
+typedef union tp_code {
+    unsigned char i;
+    struct { unsigned char i,a,b,c; } regs;
+    struct { char val[4]; } string;
+    struct { float val; } number;
+} tp_code;
+
+typedef struct tp_frame_ {
+    tp_obj code;
+    tp_code *cur;
+    tp_code *jmp;
+    tp_obj *regs;
+    tp_obj *ret_dest;
+    tp_obj fname;
+    tp_obj name;
+    tp_obj line;
+    tp_obj globals;
+    int lineno;
+    int cregs;
+} tp_frame_;
+
+#define TP_GCMAX 4096
+#define TP_FRAMES 256
+#define TP_REGS_EXTRA 2
+/* #define TP_REGS_PER_FRAME 256*/
+#define TP_REGS 16384
+
+typedef struct tp_vm {
+    tp_obj builtins;
+    tp_obj modules;
+    tp_frame_ frames[TP_FRAMES];
+    tp_obj _params;
+    tp_obj params;
+    tp_obj _regs;
+    tp_obj *regs;
+    tp_obj root;
+    jmp_buf buf;
+#ifdef CPYTHON_MOD
+    jmp_buf nextexpr;
+#endif
+    int jmp;
+    tp_obj ex;
+    char chars[256][2];
+    int cur;
+    _tp_list *white;
+    _tp_list *grey;
+    _tp_list *black;
+    int steps;
+    clock_t clocks;
+    double time_elapsed;
+    double time_limit;
+    unsigned long mem_limit;
+    unsigned long mem_used;
+    int mem_exceeded;
+} tp_vm;
+
+#define TP tp_vm *tp
+typedef struct _tp_data {
+    int gci;
+    void (*free)(TP,tp_obj);
+} _tp_data;
+
+#define tp_True tp_number(1)
+#define tp_False tp_number(0)
+
+extern tp_obj tp_None;
+
+#ifdef TP_SANDBOX
+void *tp_malloc(TP, unsigned long);
+void *tp_realloc(TP, void *, unsigned long);
+void tp_free(TP, void *);
+#else
+#define tp_malloc(TP,x) calloc((x),1)
+#define tp_realloc(TP,x,y) realloc(x,y)
+#define tp_free(TP,x) free(x)
+#endif
+
+void tp_sandbox(TP, double, unsigned long);
+void tp_time_update(TP);
+void tp_mem_update(TP);
+
+void tp_run(TP,int cur);
+void tp_set(TP,tp_obj,tp_obj,tp_obj);
+tp_obj tp_get(TP,tp_obj,tp_obj);
+tp_obj tp_has(TP,tp_obj self, tp_obj k);
+tp_obj tp_len(TP,tp_obj);
+void tp_del(TP,tp_obj,tp_obj);
+tp_obj tp_str(TP,tp_obj);
+int tp_bool(TP,tp_obj);
+int tp_cmp(TP,tp_obj,tp_obj);
+void _tp_raise(TP,tp_obj);
+tp_obj tp_printf(TP,char const *fmt,...);
+tp_obj tp_track(TP,tp_obj);
+void tp_grey(TP,tp_obj);
+tp_obj tp_call(TP, tp_obj fnc, tp_obj params);
+tp_obj tp_add(TP,tp_obj a, tp_obj b) ;
+
+
+#define tp_raise(r,v) { \
+    _tp_raise(tp,v); \
+    return r; \
+}
+
+
+tp_inline static tp_obj tp_string(char const *v) {
+    tp_obj val;
+    tp_string_ s = {TP_STRING, 0, v, 0};
+    s.len = strlen(v);
+    val.string = s;
+    return val;
+}
+
+#define TP_CSTR_LEN 256
+
+tp_inline static void tp_cstr(TP,tp_obj v, char *s, int l) {
+    if (v.type != TP_STRING) { 
+        tp_raise(,tp_string("(tp_cstr) TypeError: value not a string"));
+    }
+    if (v.string.len >= l) {
+        tp_raise(,tp_string("(tp_cstr) TypeError: value too long"));
+    }
+    memset(s,0,l);
+    memcpy(s,v.string.val,v.string.len);
+}
+
+
+#define TP_OBJ() (tp_get(tp,tp->params,tp_None))
+tp_inline static tp_obj tp_type(TP,int t,tp_obj v) {
+    if (v.type != t) { tp_raise(tp_None,tp_string("(tp_type) TypeError: unexpected type")); }
+    return v;
+}
+
+
+
+#define TP_NO_LIMIT 0
+#define TP_TYPE(t) tp_type(tp,t,TP_OBJ())
+#define TP_NUM() (TP_TYPE(TP_NUMBER).number.val)
+#define TP_STR() (TP_TYPE(TP_STRING))
+#define TP_DEFAULT(d) (tp->params.list.val->len?tp_get(tp,tp->params,tp_None):(d))
+
+#define TP_LOOP(e) \
+    int __l = tp->params.list.val->len; \
+    int __i; for (__i=0; __i<__l; __i++) { \
+    (e) = _tp_list_get(tp,tp->params.list.val,__i,"TP_LOOP");
+#define TP_END \
+    }
+
+tp_inline static int _tp_min(int a, int b) { return (a<b?a:b); }
+tp_inline static int _tp_max(int a, int b) { return (a>b?a:b); }
+tp_inline static int _tp_sign(tp_num v) { return (v<0?-1:(v>0?1:0)); }
+
+tp_inline static tp_obj tp_number(tp_num v) {
+    tp_obj val = {TP_NUMBER};
+    val.number.val = v;
+    return val;
+}
+
+tp_inline static void tp_echo(TP,tp_obj e) {
+    e = tp_str(tp,e);
+    fwrite(e.string.val,1,e.string.len,stdout);
+}
+
+tp_inline static tp_obj tp_string_n(char const *v,int n) {
+    tp_obj val;
+    tp_string_ s = {TP_STRING, 0,v,n};
+    val.string = s;
+    return val;
+}
+
+
+tp_obj _tp_dcall(TP,tp_obj fnc(TP)) {
+    return fnc(tp);
+}
+
+tp_obj _tp_tcall(TP,tp_obj fnc) {
+    if (fnc.fnc.ftype&2) {
+        _tp_list_insert(tp,tp->params.list.val,0,fnc.fnc.info->self);
+    }
+    return _tp_dcall(tp,(tp_obj (*)(tp_vm *))fnc.fnc.cfnc);
+}
+
+tp_obj tp_fnc_new(TP,int t, void *v, tp_obj c,tp_obj s, tp_obj g) {
+    tp_obj r = {TP_FNC};
+    _tp_fnc *info = (_tp_fnc*)tp_malloc(tp, sizeof(_tp_fnc));
+    info->code = c;
+    info->self = s;
+    info->globals = g;
+    r.fnc.ftype = t;
+    r.fnc.info = info;
+    r.fnc.cfnc = v;
+    return tp_track(tp,r);
+}
+
+tp_obj tp_def(TP,tp_obj code, tp_obj g) {
+    tp_obj r = tp_fnc_new(tp,1,0,code,tp_None,g);
+    return r;
+}
+
+tp_obj tp_fnc(TP,tp_obj v(TP)) {
+    return tp_fnc_new(tp,0,v,tp_None,tp_None,tp_None);
+}
+
+tp_obj tp_method(TP,tp_obj self,tp_obj v(TP)) {
+    return tp_fnc_new(tp,2,v,tp_None,self,tp_None);
+}
+
+tp_obj tp_data(TP,int magic,void *v) {
+    tp_obj r = {TP_DATA};
+    r.data.info = (_tp_data*)tp_malloc(tp, sizeof(_tp_data));
+    r.data.val = v;
+    r.data.magic = magic;
+    return tp_track(tp,r);
+}
+
+
+tp_obj tp_params(TP) {
+    tp_obj r;
+    tp->params = tp->_params.list.val->items[tp->cur];
+    r = tp->_params.list.val->items[tp->cur];
+    r.list.val->len = 0;
+    return r;
+}
+
+
+tp_obj tp_params_n(TP,int n, tp_obj argv[]) {
+    tp_obj r = tp_params(tp);
+    int i; for (i=0; i<n; i++) { _tp_list_append(tp,r.list.val,argv[i]); }
+    return r;
+}
+
+
+tp_obj tp_params_v(TP,int n,...) {
+    int i;
+    tp_obj r = tp_params(tp);
+    va_list a; va_start(a,n);
+    for (i=0; i<n; i++) {
+        _tp_list_append(tp,r.list.val,va_arg(a,tp_obj));
+    }
+    va_end(a);
+    return r;
+}
+
+
+#endif
