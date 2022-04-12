dabd5ee0ea6e4f27539d8767bc83bec6d4848b87
zkkxu <xufei02@pingcap.com>
2022-04-12 13:21:09 +0800

    fix

--- mypy/type.c
+++ mypy/type.c
@@ -3,12 +3,12 @@
 #endif
 
 #include "type.h"
-#include "listdict.c"
-#include "string.c"
-#include "builtins.c"
-#include "gc.c"
-#include "ops.c"
-#include "tinyvm.c"
+#include "listdict.h"
+#include "string.h"
+#include "builtins.h"
+#include "garbage.h"
+#include "operator.h"
+#include "myvm.h"
 
 void compile_code(TP);
 
@@ -16,14 +16,16 @@ ObjType NONE = {NONETYPE};
 
 #if TP_COMPILER
 #include "bc.c"
-void compile_code(TP) {
-    tp_import(tp,0,"tokenize",tp_tokenize,sizeof(tp_tokenize));
-    tp_import(tp,0,"parse",tp_parse,sizeof(tp_parse));
-    tp_import(tp,0,"encode",tp_encode,sizeof(tp_encode));
-    tp_import(tp,0,"py2bc",tp_py2bc,sizeof(tp_py2bc));
-    tp_ez_call(tp,"py2bc","_init",NONE);
+void compile_code(TP)
+{
+    tp_import(tp, 0, "tokenize", tp_tokenize, sizeof(tp_tokenize));
+    tp_import(tp, 0, "parse", tp_parse, sizeof(tp_parse));
+    tp_import(tp, 0, "encode", tp_encode, sizeof(tp_encode));
+    tp_import(tp, 0, "py2bc", tp_py2bc, sizeof(tp_py2bc));
+    tp_ez_call(tp, "py2bc", "_init", NONE);
 }
 #else
-void compile_code(TP) { }
+void compile_code(TP)
+{
+}
 #endif
-
